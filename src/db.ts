a1a77746f3c334085585f7213f647e199be8abda
sebastian cartes <sebastiancartes.work@gamil.com>
2023-10-13 08:41:43 -0400

    chango comands

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { configs } from "./configs/config";
 
 (async () => {
   try {
-    await mongoose.connect(configs.db.uri + configs.db.name);
+    await mongoose.connect(`mongodb://${configs.db.userName}:${configs.db.password}@ac-p2o5dc8-shard-00-00.gnuqu5a.mongodb.net:27017,ac-p2o5dc8-shard-00-01.gnuqu5a.mongodb.net:27017,ac-p2o5dc8-shard-00-02.gnuqu5a.mongodb.net:27017/?ssl=true&replicaSet=atlas-kflfrt-shard-0&authSource=admin&retryWrites=true&w=majority`);
     console.log("DB is connected");
   } catch (err) {
     console.log("err in DB: ", err);
